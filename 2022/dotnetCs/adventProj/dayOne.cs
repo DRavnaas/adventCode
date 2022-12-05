70a85c449ce7d4fbbe96efd92b0b50037ef69331
DRavnaas <doylerav@gmail.com>
2022-12-05 10:05:15 -0800

    initial files for day 1 cs

---
+++ 2022/dotnetCs/adventProj/dayOne.cs
@@ -0,0 +1,44 @@
+namespace adventProj
+{
+    internal class DayOne
+    {
+        internal static uint CountElfCalories(string elfCalorieInput, uint maxLines=uint.MaxValue)
+        {
+            Console.WriteLine("counting calories from:\n{0}", elfCalorieInput);
+
+            // read block of numbers, keep track of the largest block
+            string [] elfCalorieLines = elfCalorieInput.Split("\n");
+            uint maxCalories = 0, currentCalories = 0, currLine = 0;
+            foreach (string input in elfCalorieLines)
+            {
+                if (currLine >= maxLines)
+                {
+                    break;
+                }
+                if (!string.IsNullOrWhiteSpace(input))
+                {
+                    // Should be a number.  Ignore if not (ie: zero calories)
+                    uint calorieValue = 0;
+                    if (uint.TryParse(input, out calorieValue))
+                    {
+                        currentCalories += calorieValue;
+                    }
+                }
+                else
+                // elf calorie block separator?
+                {
+                    if (currentCalories > maxCalories)
+                    {
+                        Console.WriteLine("new max = {0}", currentCalories);
+                        maxCalories = currentCalories;
+                    }
+
+                    // Clear out current elf calorie counter for the next one
+                    currentCalories = 0;
+                }
+            }
+
+            return maxCalories;
+        }
+    }
+}

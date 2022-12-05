111dd8746ddfd1e6f2bf97c78608348401976d0f
DRavnaas <doylerav@gmail.com>
2022-12-05 10:46:44 -0800

    day one solution with input

--- 2022/dotnetCs/adventProj/Program.cs
+++ 2022/dotnetCs/adventProj/Program.cs
@@ -7,9 +7,27 @@
             string testInput = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000";
             uint testAnswer = 24000;
 
+            if (args.Any())
+            {
+                Console.WriteLine("Reading input from {0}", args[0]);
+                testInput = ReadInputToText(args[0]);
+            }
+
             uint mostCalories = DayOne.CountElfCalories(testInput);
 
             Console.WriteLine("\nMost calories: {0}\nExpected answer: {1}", mostCalories, testAnswer);
+        }
+
+        static string ReadInputToText(string filename)
+        {
+            string retVal = String.Empty;
+
+            if (!String.IsNullOrWhiteSpace(filename) && System.IO.File.Exists(filename))
+            {
+                retVal = System.IO.File.ReadAllText(filename);
+            }
+
+            return retVal;
         }
     }
 }
--- 2022/dotnetCs/adventProj/dayOne.cs
+++ 2022/dotnetCs/adventProj/dayOne.cs
@@ -2,19 +2,21 @@ namespace adventProj
 {
     internal class DayOne
     {
-        internal static uint CountElfCalories(string elfCalorieInput, uint maxLines=uint.MaxValue)
+        internal static uint CountElfCalories(string elfCalorieInput, uint maxLinesToRead=uint.MaxValue)
         {
-            Console.WriteLine("counting calories from:\n{0}", elfCalorieInput);
+            //Console.WriteLine("counting calories from:\n{0}", elfCalorieInput);
 
             // read block of numbers, keep track of the largest block
             string [] elfCalorieLines = elfCalorieInput.Split("\n");
             uint maxCalories = 0, currentCalories = 0, currLine = 0;
             foreach (string input in elfCalorieLines)
             {
-                if (currLine >= maxLines)
+                if (currLine >= maxLinesToRead)
                 {
+                    Console.WriteLine("line max hit at {0}, exiting...", currLine);
                     break;
                 }
+                currLine++;
 
                 if (!string.IsNullOrWhiteSpace(input))
                 {
@@ -25,20 +27,21 @@ namespace adventProj
                         currentCalories += calorieValue;
                     }
                 }
-                else
-                // elf calorie block separator?
-                {
-                    if (currentCalories > maxCalories)
-                    {
-                        Console.WriteLine("new max = {0}", currentCalories);
-                        maxCalories = currentCalories;
-                    }
+                else {
 
                     // Clear out current elf calorie counter for the next one
                     currentCalories = 0;
                 }
-            }
 
+                    if (currentCalories > maxCalories)
+                    {
+                        //Console.WriteLine("new max = {0}", currentCalories);
+                        maxCalories = currentCalories;
+                    }
+
+                
+            
+            }
             return maxCalories;
         }
     }

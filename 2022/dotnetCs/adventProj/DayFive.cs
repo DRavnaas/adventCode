20608bfa9c6a8b54fd49d47b12560772d3939cf9
DRavnaas <doylerav@gmail.com>
2022-12-06 15:50:31 -0800

    day 5 part two complete

--- 2022/dotnetCs/adventProj/DayFive.cs
+++ 2022/dotnetCs/adventProj/DayFive.cs
@@ -18,9 +18,9 @@ namespace adventProj
             {
                 Name = name;
             }
-            public static Stack<Crate>[] BuildStacks(string crateStacksTextLines)
+            public static List<Crate>[] BuildStacks(string crateStacksTextLines)
             {
-                Stack<Crate>[] stackOfCrates;
+                List<Crate>[] stackOfCrates;
 
                 string[] stackHorizontal = crateStacksTextLines.Split("\n");
                 string[] stackLastLine = stackHorizontal.Last().Split(" ");
@@ -36,7 +36,7 @@ namespace adventProj
                 }
 
                 // Now build the actual stacks
-                stackOfCrates = new Stack<Crate>[columnCounts];
+                stackOfCrates = new List<Crate>[columnCounts];
                 foreach (string cratesInput in stackHorizontal.SkipLast(1).Reverse())
                 {
                     if (!String.IsNullOrWhiteSpace(cratesInput))
@@ -44,18 +44,20 @@ namespace adventProj
                         // walk horizontal row by # of crates
                         // (this assumes crate keys are in order they were added - check!)
                         int crateInputIndex = 0;
-                        for(int i=0; i < columnCounts; i++)
+                        for (int i = 0; i < columnCounts; i++)
                         {
+                            // Start each stack off with an empty list (no crates)
+                            if (stackOfCrates[i] == null)
+                            {
+                                stackOfCrates[i] = new List<Crate>();
+                            }
+
                             if (cratesInput[crateInputIndex] == '[')
                             {
-                                // Push this crate onto the appropriate stack
+                                // Add this crate to the appropriate stack
                                 var currentCrate = new Crate(cratesInput[crateInputIndex + 1].ToString());
 
-                                if (stackOfCrates[i] == null)
-                                {
-                                    stackOfCrates[i] = new Stack<Crate>();
-                                }
-                                stackOfCrates[i].Push(currentCrate);
+                                stackOfCrates[i].Add(currentCrate);
                             }
 
                             // Advance a column
@@ -68,25 +70,25 @@ namespace adventProj
                 return stackOfCrates;
             }
 
-            public static string GetTopCrateNames(Stack<Crate>[] crateStacks)
+            public static string GetTopCrateNames(List<Crate>[] crateStacks)
             {
                 // Get the top crates after moves
-                        string tmpVal = "";
-                        foreach (Stack<Crate> stack in crateStacks)
-                        {
-                            if (stack.Count() == 0)
-                            {
-                                // empty stack
-                                tmpVal += " ";
-                            }
-                            else
-                            {
-                                tmpVal += (stack.Peek().Name);
-                            }
+                string tmpVal = "";
+                foreach (List<Crate> stack in crateStacks)
+                {
+                    if (stack.Count() == 0)
+                    {
+                        // empty stack
+                        tmpVal += " ";
+                    }
+                    else
+                    {
+                        tmpVal += (stack.Last().Name);
+                    }
 
-                        }
+                }
 
-                        return tmpVal;
+                return tmpVal;
 
             }
 
@@ -96,10 +98,10 @@ namespace adventProj
         {
             string retVal = "";
 
-            //if (String.IsNullOrEmpty(testInput))
+            if (String.IsNullOrEmpty(testInput))
             {
                 // part 1: this result is CMP; test input file result MQTPGLLDN
-                // pars 2: this result is MCD; test input file result 
+                // part 2: this result is MCD; test input file result 
                 testInput = "    [D]    \n[N] [C]    \n[Z] [M] [P] \n 1   2   3 \n\n" +
                 "move 1 from 2 to 1\n" +
                 "move 3 from 1 to 3\n" +
@@ -109,32 +111,38 @@ namespace adventProj
 
             string[] cratesAndMoves = testInput.Split("\n\n");
 
-            Stack<Crate>[] crateStacks = Crate.BuildStacks(cratesAndMoves[0]);
+            List<Crate>[] crateStacks = Crate.BuildStacks(cratesAndMoves[0]);
 
             string[] moves = cratesAndMoves[1].Split("\n");
             foreach (string moveDetails in moves)
             {
                 string[] moveParameters = moveDetails.Split(" ");
 
-                var tmpVal = Crate.GetTopCrateNames(crateStacks);
-                        
-                Console.WriteLine($"top of stack = {tmpVal} before action: {moveDetails}");
-
                 // process moves by popping/pushing
                 // index 1 = # crates, index 3 = from stack key, index 5 = to stack key
                 if (moveParameters.Count() == 6)
                 {
+                    var tmpVal = Crate.GetTopCrateNames(crateStacks);
+
+                    Console.WriteLine($"top of stack = {tmpVal} before action: {moveDetails}");
+
                     int numCrates = 0;
                     if (int.TryParse(moveParameters[1], out numCrates))
                     {
-                        int fromStack = int.Parse(moveParameters[3]) -1 ;
+                        int fromStack = int.Parse(moveParameters[3]) - 1;
                         int toStack = int.Parse(moveParameters[5]) - 1;
 
-                        for (int i = 1; i < numCrates + 1; i++)
-                        {
-                            var crateMoved = crateStacks[fromStack].Pop();
-                            crateStacks[toStack].Push(crateMoved);
-                        }
+                        // Copy crates from one stack to other, then delete from original stack
+                        //for (int i= crateStacks[fromStack].Count()-numCrates; i<crateStacks[fromStack].Count(); i++)
+                        //{
+                        var cratesToMove = crateStacks[fromStack].GetRange(crateStacks[fromStack].Count()-numCrates, numCrates);
+                        crateStacks[toStack].AddRange(cratesToMove);
+                        crateStacks[fromStack].RemoveRange(crateStacks[fromStack].Count()-numCrates, numCrates);
+                        //for (int i = 1; i < numCrates + 1; i++)
+                        //{
+                        //    var crateMoved = crateStacks[fromStack].Pop();
+                        //    crateStacks[toStack].Push(crateMoved);
+                        //}
                     }
                 }
             }
